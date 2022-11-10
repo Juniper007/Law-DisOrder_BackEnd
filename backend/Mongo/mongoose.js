e1fdd86ee55d8b2f8f8889bb349b17038736d1c4
Maggie Chew <maggie.chew.89@gmail.com>
2022-11-09 20:33:13 -0700

    The night of a million updates

---
+++ backend/Mongo/mongoose.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+
+mongoose.connect('mongodb+srv://MAdminChew:SbTm0D9s4zMNyguB@cluster0.fazjowh.mongodb.net/?retryWrites=true&w=majority') // UPDATE
+export default mongoose;
