d3ecaf597febb8ea0981c250473fe5e5c8fb2a8d
Maggie Chew <maggie.chew.89@gmail.com>
2022-11-10 22:48:53 -0700

    figuring out update process

---
+++ backend/src/Mongo/adding-tables.js
@@ -0,0 +1,16 @@
+import { SourceTable } from "./Update_model";
+
+let sampleDataset = {
+    sourceName: "TestName",
+    dateLastUpdated: "TestDate",
+    url:"www.example.mag"
+}
+
+let array = [sampleDataset];
+
+
+function updateTable (array){
+    SourceTable.addNewDatasets()
+    
+    return 
+}
