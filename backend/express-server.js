a766415833660cad2cfec820efaf6be4e4885a2d
Maggie Chew <maggie.chew.89@gmail.com>
2022-11-18 10:43:08 -0700

    Updated Express

---
+++ backend/express-server.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+import myConfig from 'dotenv';
+myConfig.config();
+import DEBUG from 'debug';
+import userRouter from './routes/userRouter.js'
+
+export const debug = DEBUG('server:routes');
+debug.enabled = true;
+const port = 5000;
+const app = express();
+app.use(express.json());
+
+
+
+
+app.get('/slow', (req, res) => {
+    console.log('delay for 3 seconds')
+    setTimeout(() => {
+        let seconds = new Date().getTime();
+        res.send({currentTime: seconds})
+    }, 3000)
+});
+
+app.use("/api/user", userRouter)
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
