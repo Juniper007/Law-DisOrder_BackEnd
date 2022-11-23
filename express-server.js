9db6e8fc9127426183105546411b747f608b1ba5
Maggie Chew <maggie.chew.89@gmail.com>
2022-11-23 14:38:44 -0700

    Updated files

--- express-server.js
+++ express-server.js
@@ -1,14 +1,14 @@
 import express from 'express';
-// import myConfig from 'dotenv';
+import myConfig from 'dotenv';
 import DEBUG from 'debug';
 
 import userRouter from './routes/userRouter.js';
 import dataRouter from './routes/dataRouter.js';
 
-// myConfig.config();
+myConfig.config();
 export const debug = DEBUG('express-server:routes');
 debug.enabled = true;
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
